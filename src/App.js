0a518040a942a5e18aba8f5b582a8652e44aa927
Kevin Hernandez <kev.hdez05@gmail.com>
2022-02-24 10:22:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense, useState } from 'react';
+
+//Components
+import Navbar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
+// import Post from './components/post/post';
+
+//konami code
+import KonamiCode from './components/konamicode/konamiCode';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-header">
+       
+        <Navbar name="Kevin Hernandez - Blog" />
+        <KonamiCode courseKey={'null'}/>
+        
+
+        
+        <Footer />
+      </main>
     </div>
+
+
   );
 }
+
+
 
 export default App;
