440f58076da33c2f53eea0340baeb2836a0c6b9b
Kevin Hernandez <kev.hdez05@gmail.com>
2022-03-28 08:05:48 -0400

    selectable div, just keeping p tags on card's description, hover changing color and transparency

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,11 +2,13 @@ import React from "react";
 import Markdown from "markdown-to-jsx";
 import formatDate from "../../Utils/dateFormat";
 
+import './styles.css';
+
 const Card = ({ post }) => {
   return (
     <div className="container sm:px-32 md:px-42 xl:px-64 mx-auto">
       <div className="p-4">
-        <div className="h-full border-2 border-gray-200 rounded-lg overflow-hidden grid-cols-1 lg:grid-cols-2">
+        <div className="card h-full border-2 border-gray-200 rounded-lg overflow-hidden grid-cols-1 lg:grid-cols-2">
           {/* <img
             className="h-full w-full object-cover object-center"
             src="https://camo.githubusercontent.com/cc42c1358f33e518bb1f3353bd1ce7984940b49635943a409cd45521c9c6ca8e/68747470733a2f2f63646e2d696d616765732d312e6d656469756d2e636f6d2f6d61782f3830302f302a566661486b57627144757068725171582e6a7067"
@@ -27,10 +29,12 @@ const Card = ({ post }) => {
               </h1>
             </div>
 
-            {/* Information */}
-            <h1 className="tracking-widest text-lg title-font font-medium text-gray-500 mb-1">
+            {/* Card's information */}
+            <h1 className="card-description tracking-widest text-lg title-font font-medium text-gray-400 mb-1">
               <Markdown>{post.body}</Markdown>
             </h1>
+
+            {/* Published date */}
             <div className="flex items-center flex-wrap ">
               <span className="text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-300">
                 {formatDate(
